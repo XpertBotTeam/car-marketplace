b003417444766ecd3c6ef6caba312aeec75d526e
mmchokor <mohameddd701@gmail.com>
2022-07-24 23:57:10 +0300

    Installed MaterialUI & started working on header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/layout/Header'
+import Home from './pages/Home'
+import Box from '@mui/material/Box'
+
 function App() {
-   return <></>
+   return (
+      <>
+         <Router>
+            <Header />
+            <Routes>
+               <Route path='/' element={<Home />} />
+            </Routes>
+         </Router>
+      </>
+   )
 }
 
 export default App
