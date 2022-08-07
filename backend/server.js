1d6a066f149301d65e6552f9ef7c74486448c2ab
mmchokor <mohameddd701@gmail.com>
2022-08-07 21:11:39 +0300

    added backend and database connection to mongoose

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 5000
+const cors = require('cors')
+const path = require('path')
+
+// connect to db
+connectDB()
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+   res.status(200).json({ message: 'Welcome to support desk API' })
+})
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT, () => {
+   console.log(`Server started on port ${PORT}`)
+})
